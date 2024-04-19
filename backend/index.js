91d62339c39a032e946fb13217eea29c9ea4cea3
AticeRuan <104746446+AticeRuan@users.noreply.github.com>
2024-04-19 16:36:21 +1200

    Add files via upload

---
+++ backend/index.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+const express = require('express');
+const workoutRoutes= require('./routes/workouts');
+const userRoutes = require('./routes/user');
+const mongoose = require('mongoose');
+const cors = require('cors');
+//express app
+const app = express();
+
+app.use(express.json());
+app.use(cors({ origin: 'http://localhost:3000',
+credentials: true,
+methods: ['GET','POST','HEAD','PUT','PATCH','DELETE'],}));
+
+//routes
+app.use('/api/workouts', workoutRoutes);
+app.use('/api/user', userRoutes);
+
+
+//connect to mongodb
+mongoose.connect(process.env.MONGO_URI)
+.then(()=>{
+    //listen for requests
+app.listen(process.env.PORT ||4000,()=>{
+    console.log(`Server is running on port ${process.env.PORT}`);
+
+});
+})
+.catch((err)=>console.log(err));
+
+
